9c7e717e0995ef5c2368ba96461ab488fb6ac7ba
jiayaobo <januzaj2011@163.com>
2022-10-14 12:59:42 +0800

    Finished

--- src/vec.rs
+++ src/vec.rs
@@ -95,6 +95,17 @@ impl Vec3 {
         r_out_perp + r_out_parallel
     }
 
+    pub fn random_in_unit_disk() -> Vec3 {
+        let mut rng = rand::thread_rng();
+
+        loop {
+            let p = Vec3::new(rng.gen_range(-1.0..1.0), rng.gen_range(-1.0..1.0), 0.0);
+            if p.length() < 1.0 {
+                return p;
+            }
+        }
+    }
+
 
 
     pub fn format_color(self, samples_per_pixel: u64) -> String {
